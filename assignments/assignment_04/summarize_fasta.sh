a4890f15aea0a44d4c8245f749097a7527794567
Phillips_Caroline <cdphillips@bora.sciclone.wm.edu>
2026-02-25 12:20:25 -0500

    Finish script calculations

--- assignments/assignment_04/summarize_fasta.sh
+++ assignments/assignment_04/summarize_fasta.sh
@@ -2,5 +2,11 @@
 set -ueo pipefail
 
 file_name=$1
-
-echo "The total number of sequences is $(seqtk comp $1 | wc -l)"
+num_sequences=$(seqtk comp ${file_name} | wc -l)
+num_nucleotides=$(grep -v "^>" ${file_name} | tr -d "\n" | wc -c)
+seq_name_length_table=$(seqtk comp ${file_name} | cut -f1,2)
+echo "File Name: ${file_name}"
+echo "The total number of sequences is ${num_sequences}"
+echo "The total number of nucleotides is ${num_nucleotides}"
+echo "This table contains the sequence names and lengths:"
+echo "${seq_name_length_table}"
