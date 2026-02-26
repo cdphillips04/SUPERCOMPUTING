439c48f43b7566b965686e7beb4e676d26ea3d8a
Phillips_Caroline <cdphillips@bora.sciclone.wm.edu>
2026-02-26 10:49:20 -0500

    Add lesson 5 materials

---
+++ lessons/lesson_05/scripts/chop_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ueo pipefail
+
+for FWD in data/*_R1_*
+do 
+REV=${FWD/_R1_/_R2_} 
+OUT=${FWD%_L001_R1_sample.fastq}_interleaved_chop_${1}.fastq
+echo $FWD $REV $OUT
+./scripts/interleave_chop.sh $FWD $REV $OUT $1
+done
