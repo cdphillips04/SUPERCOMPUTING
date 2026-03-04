313e68717af1f7523b1789b1df8e1b354e15d3d9
Phillips_Caroline <cdphillips@bora.sciclone.wm.edu>
2026-03-04 09:12:03 -0500

    Add comments and change line

--- assignments/assignment_05/scripts/01_download_data.sh
+++ assignments/assignment_05/scripts/01_download_data.sh
@@ -2,10 +2,14 @@
 
 set -ueo pipefail
 
+# open data/raw folder
+cd ./data/raw/
+
+# get data from link
 wget https://gzahn.github.io/data/fastq_examples.tar
 
+# extract data files
 tar -xf fastq_examples.tar
 
-mv *.fastq.gz ./data/raw/
-
+# remove tar file
 rm fastq_examples.tar
