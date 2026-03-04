f96b9880c652afc69612a61b4e1faa22c70b0864
Phillips_Caroline <cdphillips@bora.sciclone.wm.edu>
2026-03-04 09:11:48 -0500

    Add comments

--- assignments/assignment_05/pipeline.sh
+++ assignments/assignment_05/pipeline.sh
@@ -2,10 +2,14 @@
 
 set -ueo pipefail
 
+# make sure script 1 is executable and run it 
 chmod +x ./scripts/01_download_data.sh
 ./scripts/01_download_data.sh
 
+# make sure script 2 is executable
 chmod +x ./scripts/02_run_fastp.sh
+
+# run script 2 for each forward file in the raw data folder
 for FWD in data/raw/*_R1_*
 do
 	./scripts/02_run_fastp.sh $FWD
