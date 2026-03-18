053b5864eaad86693a0346db22e0d83b4b4ec05c
Phillips_Caroline <cdphillips@bora.sciclone.wm.edu>
2026-03-18 00:40:25 -0400

    Change log file name

--- assignments/assignment_06/scripts/03_run_flye_conda.sh
+++ assignments/assignment_06/scripts/03_run_flye_conda.sh
@@ -13,12 +13,13 @@ mkdir ./assemblies/assembly_conda
 # run flye
 flye --nano-raw ./data/SRR33939694.fastq.gz --out-dir ./assemblies/assembly_conda --threads 6
 
-# rename fasta file
+# rename fasta file and log file
 mv ./assemblies/assembly_conda/assembly.fasta ./assemblies/assembly_conda/conda_assembly.fasta
+mv mv ./assemblies/assembly_conda/flye.log ./assemblies/assembly_conda/conda_flye.log
 
 # remove everything besides conda_assembly.fasta and flye.log
 cd ./assemblies/assembly_conda
-rm -r $(ls | grep -v -e 'conda_assembly.fasta' -e 'flye.log')
+rm -r $(ls | grep -v -e 'conda_assembly.fasta' -e 'conda_flye.log')
 cd ../..
 
 # deactivate environment
