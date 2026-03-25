022c7cadf6e40296cf103f20ea62c647ed48b13f
Phillips_Caroline <cdphillips@bora.sciclone.wm.edu>
2026-03-25 17:05:01 -0400

    Update setup script for new paths

--- assignments/assignment_07/scripts/00_setup.sh
+++ assignments/assignment_07/scripts/00_setup.sh
@@ -2,7 +2,13 @@
 
 set -ueo pipefail
 
-# make directories for data and output files
-mkdir -p ./data/clean ./data/dog_reference ./data/raw
-mkdir -p output
+# make directories for data and output files in scr10
+HW_DIR=${HOME}/scr10/assignment_7
+DATA_DIR=${HW_DIR}/data
+RAW_DIR=${DATA_DIR}/raw
+CLEAN_DIR=${DATA_DIR}/clean
+DOG_DIR=${DATA_DIR}/dog_reference
+OUTPUT_DIR=${HW_DIR}/output
 
+mkdir -p $DATA_DIR $OUTPUT_DIR
+mkdir -p $RAW_DIR $CLEAN_DIR $DOG_DIR
