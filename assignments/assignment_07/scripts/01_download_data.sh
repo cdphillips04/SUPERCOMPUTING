9b4ab80be4e8bd6ddb9db6c1ef0d952dee21beac
Phillips_Caroline <cdphillips@bora.sciclone.wm.edu>
2026-03-25 17:05:26 -0400

    Fix paths for data and output

--- assignments/assignment_07/scripts/01_download_data.sh
+++ assignments/assignment_07/scripts/01_download_data.sh
@@ -2,22 +2,30 @@
 
 set -ueo pipefail
 
+# paths to data and output directories that are in scr10
+HW_DIR=${HOME}/scr10/assignment_7
+DATA_DIR=${HW_DIR}/data
+RAW_DIR=${DATA_DIR}/raw
+CLEAN_DIR=${DATA_DIR}/clean
+DOG_DIR=${DATA_DIR}/dog_reference
+OUTPUT_DIR=${HW_DIR}/output
+
 # download data using the run table
 for f in $(cat ./data/SraRunTable.csv | cut -d "," -f 1 | tail -n +2)
 do 
-	fasterq-dump ${f} -O ./data/raw -e 16
-	gzip ./data/raw/${f}_1.fastq
-	gzip ./data/raw/${f}_2.fastq
+	fasterq-dump "${f}" -O "${RAW_DIR}" -e 16
+	gzip "${RAW_DIR}/${f}_1.fastq"
+	gzip "${RAW_DIR}/${f}_2.fastq"
 done
 
 # download dog reference genome
-datasets download genome taxon "Canis familiaris" --reference --filename ./data/dog_reference/dog_reference_genome.zip
+datasets download genome taxon "Canis familiaris" --reference --filename "${DOG_DIR}/dog_reference_genome.zip"
 
 # unzip dog file and only get fna file
-unzip -j ./data/dog_reference/dog_reference_genome.zip  "*.fna" -d ./data/dog_reference
+unzip -j "${DOG_DIR}/dog_reference_genome.zip"  "*.fna" -d "${DOG_DIR}"
 
 # remove dog zip file
-rm ./data/dog_reference/dog_reference_genome.zip
+rm ${DOG_DIR}/dog_reference_genome.zip
 
 # rename fna file
-mv ./data/dog_reference/*.fna ./data/dog_reference/dog_reference_genome.fna
+mv ${DOG_DIR}/*.fna ${DOG_DIR}/dog_reference_genome.fna
