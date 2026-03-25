56541e9509de88f8f4215faf817cc9851fdfb3ec
Phillips_Caroline <cdphillips@bora.sciclone.wm.edu>
2026-03-25 12:15:50 -0400

    Add samtools to script 3

--- assignments/assignment_07/scripts/03_map_reads.sh
+++ assignments/assignment_07/scripts/03_map_reads.sh
@@ -7,5 +7,6 @@ for f in $(cat ./data/SraRunTable.csv | cut -d "," -f 1 | tail -n +2)
 do 
 	FWDOUT=./data/clean/${f}_1_clean.fastq.gz
 	REVOUT=${FWDOUT/_1_clean.fastq.gz/_2_clean.fastq.gz}
-	bbmap.sh ref=./data/dog_reference/dog_reference_genome.fna in1=${FWDOUT} in2=${REVOUT} out=./output/${f}.sam minid=0.95
+	bbmap.sh -Xmx32g ref=./data/dog_reference/dog_reference_genome.fna in1=${FWDOUT} in2=${REVOUT} out=./output/${f}.sam minid=0.95 
+	samtools view -F 4 ./output/${f}.sam > ./output/${f}_dog-matches.sam
 done
